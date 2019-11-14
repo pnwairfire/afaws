f299a934326a4028bc72e24ee10af45db9ad1fcb
Joel Dubowy <jdubowy@gmail.com>
2019-11-14 15:11:02 -0800

    Provide config defaults for all by iam_instance_profile
    the other four fields default to empty arrays

--- afaws/config.py
+++ afaws/config.py
@@ -1,3 +1,4 @@
+import copy
 import json
 
 class Config(object):
@@ -5,20 +6,25 @@ class Config(object):
     def __init__(self, user_specified_config):
         self._set_config(user_specified_config)
 
-    REQUIRED_CONFIG_FIELDS = (
-        'iam_instance_profile', 'per_instance_security_group_rules',
-        'default_efs_volumes', 'makefile_root_dirs',
-        'docker_compose_yaml_root_dirs'
-    )
+    DEFAULTS = {
+        'iam_instance_profile': None,  # Must be supplied by user
+        'per_instance_security_group_rules': [],
+        'default_efs_volumes': [],
+        'makefile_root_dirs': [],
+        'docker_compose_yaml_root_dirs': []
+   }
 
     def _set_config(self, user_specified_config):
-        self._config = {}
-        # ignores anything that's not a required field
-        for k in (self.REQUIRED_CONFIG_FIELDS):
+        self._config = copy.deepcopy(self.DEFAULTS)
+        # ignores anything that's not a recognized field
+        for k in (self.DEFAULTS):
             val = user_specified_config.get(k)
-            if not val:
+            if val is not None:
+                self._config[k] = val
+            elif self._config[k] is None:
+                # isn't defined in either defaults or user specified config
                 raise ValueError("Missing required config field %s", k)
-            self._config[k] = val
+            # else, leave default value
 
     def __call__(self, key):
         return self._config[key]
