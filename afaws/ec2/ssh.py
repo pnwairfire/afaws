67624025b2d28f579225186f33c497766f4bf096
Joel Dubowy <jdubowy@gmail.com>
2019-11-20 13:33:38 -0800

    Update SshClient.put to support directories

--- afaws/ec2/ssh.py
+++ afaws/ec2/ssh.py
@@ -12,6 +12,7 @@ or
 """
 
 import logging
+import os
 
 import paramiko
 
@@ -46,16 +47,33 @@ class SshClient(object):
         )
 
     async def put(self, local_file_path, remote_file_path):
-        ftp_client = self.client.open_sftp()
-        await run_in_loop_executor(ftp_client.put, local_file_path,
-            remote_file_path)
-        ftp_client.close()
+        """Uploads local file(s) to remote server, recursively if passed a
+        directory.
+        """
+        sftp_client = self.client.open_sftp()
+        await self._put(sftp_client, local_file_path, remote_file_path)
+        sftp_client.close()
+
+    async def _put(self, sftp_client, local_file_path, remote_file_path):
+        if os.path.isdir(local_file_path):
+            await self.execute('mkdir {}'.format(remote_file_path))
+            for f in os.listdir(local_file_path):
+                await self._put(sftp_client,
+                    os.path.join(local_file_path, f),
+                    os.path.join(remote_file_path, f))
+        else:
+            await run_in_loop_executor(sftp_client.put, local_file_path,
+                remote_file_path)
 
     async def get(self, remote_file_path, local_file_path):
-        ftp_client=ssh_client.open_sftp()
-        await run_in_loop_executor(ftp_client.get, remote_file_path,
+        """Downloads remote files to local file system
+
+        TODO: Support recusrive mode if passed a directory
+        """
+        sftp_client=ssh_client.open_sftp()
+        await run_in_loop_executor(sftp_client.get, remote_file_path,
             local_file_path)
-        ftp_client.close()
+        sftp_client.close()
 
     def close(self):
         if self.client:
